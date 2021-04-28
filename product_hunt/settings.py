a62830049fb69aa777a91fbdd394564ffd44dbba
jkeary <jkeary@gmail.com>
2021-04-28 17:02:55 -0400

    update the database to postgres

--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'product_hunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthuntdb',
+        'USER': 'postgres',
+        'PASSWORD': 'abcd1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
